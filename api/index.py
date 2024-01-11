45bccc7e56ed4e193a8d5c5c3dbf1c270e600f54
national-salvation-party <156296708+national-salvation-party@users.noreply.github.com>
2024-01-11 17:51:16 +0900

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from urllib import request
+import datetime
+import random
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def attack():
+    targreq=request.Request(os.getenv('TARGET_URL'))
+    for i in range(3):
+        response=request.urlopen(targreq)
+        #print(response)
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run()

